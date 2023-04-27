607c7f51a2c3600cad601103296327a9c1e18b77
Jesus Garcia <jesus_heavy19@hotmail.com>
2023-04-26 18:42:58 -0600

    Crear y editar heroe, snackbar

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-page',
@@ -6,11 +13,83 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
+
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superhero: new FormControl<string>('', {nonNullable:true}),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
 
   public publishers = [
     { id:'DC Comics', desc: 'DC - Comics' },
     { id:'Marvel Comics', desc: 'Marvel - Comics' }
   ]
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private snackbar: MatSnackBar
+  ){}
+  
+  ngOnInit(): void {
+    if(!this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.heroesService.getHeroById(id))
+      ).subscribe( hero => {
+        if(!hero) return this.router.navigateByUrl('/')
+
+        this.heroForm.reset( hero)
+        return;
+      });
+
+  }
+
+  get currentHero(): Hero{
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit(){
+    if(!this.heroForm.valid) return;
+
+    if(this.currentHero.id){
+      this.updateHero();
+      return;
+    } 
+
+    this.saveHero()
+    
+  }
+
+  saveHero(): void {
+    this.heroesService.addHero(this.currentHero).subscribe({
+      next: hero => {
+        this.router.navigate(['/heroes/edit/', hero.id])
+        this.showSnackbar(`${hero.superhero} creado!`);
+        console.log(hero);
+      }
+    });
+  }
+
+  updateHero(): void {
+    this.heroesService.updateHero(this.currentHero).subscribe({
+      next: hero => {
+        this.showSnackbar(`${hero.superhero} actualizado!`);
+        console.log(hero);
+      }
+    });
+  }
+
+  showSnackbar( message: string): void {
+    this.snackbar.open(message, 'ok', { duration: 2500})
+  }
 
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/hero.interface';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { environments } from 'src/environments/environments';
 
 @Injectable({providedIn: 'root'})
@@ -25,5 +25,22 @@ export class HeroesService {
     getSuggestions(query: string): Observable<Hero[]> {
         return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limits=6`);
     }
+
+    addHero(hero: Hero): Observable<Hero>{
+        return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+    }
     
+    updateHero(hero: Hero): Observable<Hero>{
+        if(!hero.id) throw Error("id es requerido")
+        return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+    }
+
+    deleteHeroById(id: string): Observable<boolean>{
+        if(!id) throw Error("id es requerido")
+        return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+            .pipe(
+                catchError(error => of(false)),
+                map( response => true)
+            );
+    }
 }
